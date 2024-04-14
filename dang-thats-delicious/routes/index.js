72eea0b8531d85e188ae463143818f0a648ff012
ezgaro <ezgaro2005@gmail.com>
2024-04-14 14:08:56 +0300

    Adding the login/register and authentication

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const storeController = require("../controllers/storeController");
 const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
 const router = express.Router();
 
 const { catchErrors } = require("../handlers/errorHandlers");
@@ -26,5 +27,15 @@ router.get('/tags',catchErrors(storeController.getStoresByTag))
 router.get('/tags/:tag',catchErrors(storeController.getStoresByTag))
 
 router.get('/login',userController.loginForm)
+router.get('/register',userController.registerForm)
+
+// 1. Validate the registration data
+// 2. Register the user
+// 3. Log them in
+router.post('/register',
+  userController.validateRegister,
+  catchErrors(userController.register),
+  authController.login
+)
 
 module.exports = router;
